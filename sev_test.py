311176c5b0d8c13e89221c57fef9396091a1bbc7
severine <severine.atis@gmail.com>
2016-12-09 15:59:42 -0500

    1 step toward channel splitting

--- sev_test.py
+++ sev_test.py
@@ -2,19 +2,42 @@ from ij import IJ
 from ij.io import FileSaver
 from os import path
 
-imp = IJ.getImage()
-fs = FileSaver(imp)
+#imp = IJ.getImage()
+#fs = FileSaver(imp)
 #print imp
 #ImagePlus, ImageSatck, CompositeImage
 
-folder = "/home/severine/Documents/processed_images"
+image = IJ.getImage()
 
-if path.exists(folder) and path.isdir(folder):
-	print "folder exists:", folder
-	filepath = path.join(folder, "test.tif")
-	if path.exists(filepath):
-		print "file exists"
-	elif fs.saveAsTiff(filepath):
-		print "saved", filepath
-else:
-	print "folder does not exist"
+fluor = image.duplicate()
+brightfield = image.duplicate()
+
+# Work on fluorescence first: assumes brightfield is the first slice
+stack = fluor.getStack()
+stack.deleteSlice(1)
+fluor.setStack(stack)
+
+IJ.run(fluor, 'Make Composite', '')
+IJ.run(fluor, 'Flatten', '')
+
+# Work on brightfield now
+stack = brightfield.getStack()
+num_slices = stack.getSize()
+for i in range(num_slices -1):
+	stack.deleteLastSlice()
+brightfield.setStack(stack)
+
+fluor.show()
+brightfield.show()
+
+#folder = "/home/severine/Documents/processed_images"
+
+#if path.exists(folder) and path.isdir(folder):
+#	print "folder exists:", folder
+#	filepath = path.join(folder, "test.tif")
+#	if path.exists(filepath):
+#		print "file exists"
+#	elif fs.saveAsTiff(filepath):
+#		print "saved", filepath
+#else:
+#	print "folder does not exist"
